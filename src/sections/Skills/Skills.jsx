6b24362d2edfdaaf6d0aba0ecceb4a931dd5d94c
Joel-Persson1 <jolle.ov@gmail.com>
2025-04-15 14:33:47 +0200

    Added more skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,21 +13,31 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        {/* <SkillList src={checkMarkIcon} skill="TypeScript" /> */}
         <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Javascript" />
+        <SkillList src={checkMarkIcon} skill="CSS / Tailwind" />
+        {/* <SkillList src={checkMarkIcon} skill="TypeScript" /> */}
+        <SkillList src={checkMarkIcon} skill="HTML" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
         <SkillList src={checkMarkIcon} skill="SQL" />
-        <SkillList src={checkMarkIcon} skill="React Router" />
-        {/* <SkillList src={checkMarkIcon} skill="React Query" /> */}
-        <SkillList src={checkMarkIcon} skill="Redux" />
+        <SkillList src={checkMarkIcon} skill="RESTful API" />
+        <SkillList src={checkMarkIcon} skill="C#" />
       </div>
       <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="SEO" />
+        <SkillList src={checkMarkIcon} skill="Agile development" />
+        <SkillList src={checkMarkIcon} skill="Scrum" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Redux" />
+        <SkillList src={checkMarkIcon} skill="React Router" />
+      </div>
     </section>
   );
 }
